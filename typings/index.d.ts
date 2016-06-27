2dac6254a5534863fa69e02ed540bafc3525be21
hrl7 <hrlclb@gmail.com>
2016-06-27 19:14:28 +0900

    Add typescript

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/debug/index.d.ts" />
+/// <reference path="globals/java/index.d.ts" />
+/// <reference path="globals/noble/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
